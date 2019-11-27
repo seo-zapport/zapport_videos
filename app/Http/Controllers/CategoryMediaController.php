37ff1592a2b6291e92b1d5ba4dce7a7377dce00e
Lawrence <pagellicious@gmail.com>
2019-11-27 14:21:54 +0800

    update gate

--- app/Http/Controllers/CategoryMediaController.php
+++ app/Http/Controllers/CategoryMediaController.php
@@ -6,6 +6,7 @@ use App\Media;
 use App\Category;
 use App\Category_media;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
 
 class CategoryMediaController extends Controller
